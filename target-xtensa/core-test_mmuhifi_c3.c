deedc597893169023cab78bef9f49d6fd41f1c86
Max Filippov <jcmvbkbc@gmail.com>
2011-12-04 04:45:30 +0400

    target-xtensa: add test_mmuhifi_c3 core
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ target-xtensa/core-test_mmuhifi_c3.c
@@ -0,0 +1,27 @@
+#include "cpu.h"
+#include "exec/exec-all.h"
+#include "exec/gdbstub.h"
+#include "qemu/host-utils.h"
+
+#include "core-test_mmuhifi_c3/core-isa.h"
+#include "overlay_tool.h"
+
+static const XtensaConfig test_mmuhifi_c3 = {
+    .name = "test_mmuhifi_c3",
+    .options = XTENSA_OPTIONS,
+    .gdb_regmap = {
+        .num_regs = 107,
+        .num_core_regs = 62,
+        .reg = {
+#include "core-test_mmuhifi_c3/gdb-config.c"
+        }
+    },
+    .nareg = XCHAL_NUM_AREGS,
+    .ndepc = 1,
+    EXCEPTIONS_SECTION,
+    INTERRUPTS_SECTION,
+    TLB_SECTION,
+    .clock_freq_khz = 10000,
+};
+
+REGISTER_CORE(test_mmuhifi_c3)
