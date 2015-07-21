ffe34597719ddaf0dce0752dfead9a0eec954e9e
Max Filippov <jcmvbkbc@gmail.com>
2015-07-21 19:32:54 +0300

    target-xtensa: implement postinc variants of FP load/store
    New FPU has LSIP/LSXP/SSIP/SSXP instructions instead of
    LSIU/LSXU/SSIU/SSXU with the same opcode. Overlay symbol
    XCHAL_HAVE_FP_POSTINC determines whether post-increment or pre-increment
    version is implemented.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- target-xtensa/translate.c
+++ target-xtensa/translate.c
@@ -1916,24 +1916,33 @@ static void disas_xtensa_insn(CPUXtensaState *env, DisasContext *dc)
         case 8: /*LSCXp*/
             switch (OP2) {
             case 0: /*LSXf*/
-            case 1: /*LSXUf*/
+            case 1: /*LSXUf*/ /*LSXPf*/
             case 4: /*SSXf*/
-            case 5: /*SSXUf*/
+            case 5: /*SSXUf*/ /*SSXPf*/
                 HAS_OPTION(XTENSA_OPTION_FP_COPROCESSOR);
                 if (gen_window_check2(dc, RRR_S, RRR_T) &&
                     gen_check_cpenable(dc, 0)) {
                     TCGv_i32 addr = tcg_temp_new_i32();
+                    TCGv_i32 vaddr = tcg_temp_new_i32();
+
                     tcg_gen_add_i32(addr, cpu_R[RRR_S], cpu_R[RRR_T]);
-                    gen_load_store_alignment(dc, 2, addr, false);
-                    if (OP2 & 0x4) {
-                        tcg_gen_qemu_st32(cpu_FR[RRR_R], addr, dc->cring);
+                    if ((OP2 & 0x1) &&
+                        option_enabled(dc, XTENSA_OPTION_FP_POSTINC)) {
+                        tcg_gen_mov_i32(vaddr, cpu_R[RRI8_S]);
                     } else {
-                        tcg_gen_qemu_ld32u(cpu_FR[RRR_R], addr, dc->cring);
+                        tcg_gen_mov_i32(vaddr, addr);
+                    }
+                    gen_load_store_alignment(dc, 2, vaddr, false);
+                    if (OP2 & 0x4) {
+                        tcg_gen_qemu_st32(cpu_FR[RRR_R], vaddr, dc->cring);
+                    } else {
+                        tcg_gen_qemu_ld32u(cpu_FR[RRR_R], vaddr, dc->cring);
                     }
                     if (OP2 & 0x1) {
                         tcg_gen_mov_i32(cpu_R[RRR_S], addr);
                     }
                     tcg_temp_free(addr);
+                    tcg_temp_free(vaddr);
                 }
                 break;
 
@@ -2550,23 +2559,32 @@ static void disas_xtensa_insn(CPUXtensaState *env, DisasContext *dc)
         switch (RRI8_R) {
         case 0: /*LSIf*/
         case 4: /*SSIf*/
-        case 8: /*LSIUf*/
-        case 12: /*SSIUf*/
+        case 8: /*LSIUf*/ /*LSIPf*/
+        case 12: /*SSIUf*/ /*SSIPf*/
             HAS_OPTION(XTENSA_OPTION_FP_COPROCESSOR);
             if (gen_window_check1(dc, RRI8_S) &&
                 gen_check_cpenable(dc, 0)) {
                 TCGv_i32 addr = tcg_temp_new_i32();
+                TCGv_i32 vaddr = tcg_temp_new_i32();
+
                 tcg_gen_addi_i32(addr, cpu_R[RRI8_S], RRI8_IMM8 << 2);
-                gen_load_store_alignment(dc, 2, addr, false);
-                if (RRI8_R & 0x4) {
-                    tcg_gen_qemu_st32(cpu_FR[RRI8_T], addr, dc->cring);
+                if ((RRI8_R & 0x8) &&
+                    option_enabled(dc, XTENSA_OPTION_FP_POSTINC)) {
+                    tcg_gen_mov_i32(vaddr, cpu_R[RRI8_S]);
                 } else {
-                    tcg_gen_qemu_ld32u(cpu_FR[RRI8_T], addr, dc->cring);
+                    tcg_gen_mov_i32(vaddr, addr);
+                }
+                gen_load_store_alignment(dc, 2, vaddr, false);
+                if (RRI8_R & 0x4) {
+                    tcg_gen_qemu_st32(cpu_FR[RRI8_T], vaddr, dc->cring);
+                } else {
+                    tcg_gen_qemu_ld32u(cpu_FR[RRI8_T], vaddr, dc->cring);
                 }
                 if (RRI8_R & 0x8) {
                     tcg_gen_mov_i32(cpu_R[RRI8_S], addr);
                 }
                 tcg_temp_free(addr);
+                tcg_temp_free(vaddr);
             }
             break;
 
